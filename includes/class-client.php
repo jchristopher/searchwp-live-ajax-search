033e2139583a8f17c299505d6197416dd392bdb4
Jonathan Christopher <jonathan@irontoiron.com>
2014-05-02 12:19:24 -0400

    Fall back to native WordPress search if SearchWP isn't found

--- includes/class-client.php
+++ includes/class-client.php
@@ -15,7 +15,10 @@ class SearchWP_Live_Search_Client extends SearchWP_Live_Search {
 	function search() {
 		global $wpdb;
 
+		$show_results = false;
+
 		if( isset( $_REQUEST['swpengine'] ) && isset( $_REQUEST['swpquery'] ) && class_exists( 'SearchWP' ) ) {
+			$show_results = true;
 			$engine = sanitize_text_field( $_REQUEST['swpengine'] );
 			$query = sanitize_text_field( $_REQUEST['swpquery'] );
 
@@ -39,9 +42,20 @@ class SearchWP_Live_Search_Client extends SearchWP_Live_Search {
 				'post__in' => $posts,
 				'orderby' => 'post__in',
 			);
+		} elseif( isset( $_REQUEST['swpquery'] ) ) {
+			$show_results = true;
+
+			// no SearchWP, let's just fall back to a native search
+			$args = array(
+				'posts_per_page' => absint( apply_filters( 'searchwp_live_search_posts_per_page', 10 ) ),
+				's' => sanitize_text_field( $_REQUEST['swpquery'] )
+			);
+		}
+
+		if( $show_results && isset( $args ) ) {
 			query_posts( $args );
 
-			// output the results
+			// output the results using the results template
 			$results = new SearchWP_Live_Search_Template();
 			$results->get_template_part( 'search-results' );
 		}
