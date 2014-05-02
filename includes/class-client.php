fba602af3f2c6ba59bfb2571aadb260525e16689
Jonathan Christopher <jonathan@irontoiron.com>
2014-05-02 12:46:51 -0400

    Get rid of the pagination sub-function. Formatting.

--- includes/class-client.php
+++ includes/class-client.php
@@ -5,6 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 include_once( dirname( __FILE__ ) . '/class-template.php' );
 
+function my_searchwp_live_search_posts_per_page() {
+	$per_page = absint( apply_filters( 'searchwp_live_search_posts_per_page', 10 ) );
+	return $per_page;
+}
+
 class SearchWP_Live_Search_Client extends SearchWP_Live_Search {
 
 	function setup() {
@@ -18,38 +23,35 @@ class SearchWP_Live_Search_Client extends SearchWP_Live_Search {
 		$show_results = false;
 
 		if( isset( $_REQUEST['swpengine'] ) && isset( $_REQUEST['swpquery'] ) && class_exists( 'SearchWP' ) ) {
-			$show_results = true;
-			$engine = sanitize_text_field( $_REQUEST['swpengine'] );
-			$query = sanitize_text_field( $_REQUEST['swpquery'] );
 
+			// use SearchWP
+			$show_results = true;
 			$searchwp = SearchWP::instance();
 
 			// set up custom posts per page
-			function my_searchwp_live_search_posts_per_page() {
-				$per_page = absint( apply_filters( 'searchwp_live_search_posts_per_page', 10 ) );
-				return $per_page;
-			}
 			add_filter( 'searchwp_posts_per_page', 'my_searchwp_live_search_posts_per_page' );
 
 			// prevent loading Post objects, we only want IDs
 			add_filter( 'searchwp_load_posts', '__return_false' );
 
 			// grab our post IDs
-			$posts = $searchwp->search( $engine, $query );
+			$posts = $searchwp->search( sanitize_text_field( $_REQUEST['swpengine'] ), sanitize_text_field( $_REQUEST['swpquery'] ) );
 
 			// set up an environment prepared for a template part
 			$args = array(
 				'post__in' => $posts,
 				'orderby' => 'post__in',
 			);
+
 		} elseif( isset( $_REQUEST['swpquery'] ) ) {
-			$show_results = true;
 
 			// no SearchWP, let's just fall back to a native search
+			$show_results = true;
 			$args = array(
 				'posts_per_page' => absint( apply_filters( 'searchwp_live_search_posts_per_page', 10 ) ),
 				's' => sanitize_text_field( $_REQUEST['swpquery'] )
 			);
+
 		}
 
 		if( $show_results && isset( $args ) ) {
